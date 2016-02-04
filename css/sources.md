d1c71bc0f274ec6798cb659d94373bf3dd252790
audreyandoy <audreylandoy@gmail.com>
2016-02-04 09:12:30 -0800

    third commit

---
+++ css/sources.md
@@ -0,0 +1,52 @@
+1		Andre3000	http://www.people.com/people/gallery/0,,1035920_836162,00.html													
+2		2Chainz	http://www.rapbasement.com/2-chainz/011916-2-chainz-donates-house-atlanta-family-11.html													
+3		Biggie	http://hiphopdx.com/news/id.36761/title.the-notorious-b-i-g-role-cast-for-all-eyez-on-me-tupac-biopic													
+4		tupac	http://www.thewrap.com/tupac-shakur-musical-headed-broadway/													
+5		nas	http://deadline.com/2014/02/xbox-developing-1990s-comedy-series-based-on-rapper-nas-life-681025/													
+6		lauryn Hill	http://www.librarising.com/astrology/celebs/laurynhill.html													
+7		Pusha T	http://www.bet.com/topics/p/pusha-t.html													
+8		Weezy 	http://pitchfork.com/news/58255-lil-wayne-sues-birdman-and-cash-money-for-51-million-freedom-from-label/													
+9		Kendrick Lamar	http://www.nme.com/news/kendrick-lamar/87568													
+10		Common	http://www.apbspeakers.com/speaker/common													
+11		Drake	http://thatgrapejuice.net/2014/09/drake-cowriter-suggests-beyonce-nicki-minaj-ghostwriters-pen-hit-songs/													
+12		Busta Rhymes	http://www.xxlmag.com/news/2015/01/rappers-crazy-hair-transformations/						http://stackoverflow.com/questions/20694522/black-and-white-css-background-on-hover							
+13		Jadakiss	http://www.hotnewhiphop.com/Jadakiss/profile/													
+14		Eminem	http://www.howtosavemoney.com/how-rich-is-eminem/#.VrHM_zYrLVo													
+15		Missy Elliot	http://hiphopgoldenage.com/artists/missy-elliott/													
+16		Fat Joe	http://www.hotnewhiphop.com/fat-joe-games-and-things-song.1079849.html													
+17		50 Cent	http://urbanislandz.com/2014/04/16/50-cent-hustle-music-video/													
+18		Jay-Z	http://www.westword.com/music/the-best-rap-shows-in-denver-in-december-5710477													
+19		J. Cole	http://mikeepps.world/jcole-real													
+20		Big Boi 	http://www.picpicx.com/big-boi/													
+21		P.Diddy 	http://www.metrolyrics.com/puff-daddy-lyrics.html													
+22		Kanye	https://www.thinglink.com/scene/696461341266804737													
+23		E-40	http://www.mtv.com/photos/mtvcom-exclusive-e-40/1597667/3311677/photo/													
+24		Talib Kweli	http://www.rapbasement.com/talib-kweli/051013-talib-kweli-says-jay-z-biggie-nas-are-rap-nerds.html													
+25		Mos Def	https://twitter.com/mosdefofficial													
+																
+		Character	M	F	Long-hair	Short-Hair	Blck/Brwn/Blnd	mouth-closed	smiling	Sunglasses	Chains	Hat	Outerwear	Facial-Hair		
+1	a	2-chainz	Y	N	Y	N	Y	Y	N	Y	Y	Y	N	Y		
+2	b	50cent	Y	N	N	Y	Y	Y	N	N	Y	Y	N	Y		
+3	c	Andre3000	Y	N	N	Y	Y	Y	N	N	N	Y	N	Y		
+4	d	Big Boi	Y	N	N	Y	Y	Y	N	N	N	Y	Y	Y		
+5	e	Notor. B.I.G	Y	N	N	Y	Y	N	N	Y	Y	Y	N	N		
+6	f	Busta Rhymes														
+7	g	Common														
+8	h	Drake														
+9	i	E-40														
+10	j	Eminem														
+11	k	Fat Joe														
+12	l	JadaKiss														
+13	m	Jay-Z														
+14	n	J.Cole														
+15	o	Kanye West														
+16	p	Kendrick Lamar														
+17	q	Lauryn Hill														
+18	r	Missy Elliot														
+19	s	Mos Def														
+20	t	Nas														
+21	u	P. Diddy														
+22	v	Snoop Dog														
+23	w	Tupac														
+24	x	Weezy														
+																
--- js/mainBoard.js
+++ js/mainBoard.js
@@ -2,15 +2,17 @@
 var playerChar;
 var cpuChar;
 var name;
+// var oneClick = function(event){
+//                event.stopPropagation();
+//             };
 
 $(document).ready(function() {
 
 // document.addEventListener('DOMContentLoaded',function(){
 
 char = false;
-var i = 0;
 var characters = $(".col-xs-2");
-var board =$(".board");
+
 // var charList = [
 
 //   'characters/2Chainz.png','characters/50cent.png','characters/andre3000.png', 'characters/bigboi.png','characters/biggie.png','characters/busta.png',
@@ -33,195 +35,267 @@ var board =$(".board");
     // }
    
   //Player presses start button 
-      $("#start").click (function() {
-        swal({ title: "Welcome to Guess Who!", text: "To start, please choose your secret character." });
-      });
+  $("#start").click(function() {
+      swal({ title: "Welcome to Guess Who!", text: "To start, please choose your secret character." });
 
 
- //Player chooses their secret character by clicking.
-  characters.on("click", function() {
-    playerChar = $(this).attr("id");
-    name = $(this).data("name");
-    console.log($(this).attr("id"));
-    console.log($(this).data("name"));
-    chooseChar(playerChar);
-    randomCPU(cpuChar);
+     //Player chooses their secret character by clicking.
+      characters.on('click', function(e) {
 
-  });
- 
-   
- var chooseChar = function (char) {
-  // Selected Character send out sweet alert notifying the player,
-  // Keeps the character selection in the left column.
-  // Triggers the CPU random character selection.
-
-      if (char == "a") {
-          $("#playerPic").html('<img src="characters/2Chainz.png"/>');
-          swal({   title: "You picked "+ name + "!",  imageUrl: "characters/2Chainz.png"});
+         e.preventDefault;
+        
+          // if characters.attr('disabled') == "disabled" ) {
+          //   playGame() false;
+          // }
+          // else {
+          //   $(this).trigger('click');
+          // }
           
-      }                                                     // .css('background-image', 'url(' + varImagePath + ')'')
-          // $(this).attr('data-charname')
-         
-      else if (char == "b") {
-          $("#playerPic").html('<img src="characters/50cent.png"/>');
-          swal({   title: "You picked "+ name + "!", imageUrl: "characters/50cent.png" });
-         
-      }  
+          playerChar = $(this).attr("id");
+          name = $(this).data("name");
+          console.log($(this).attr("id"));
+          console.log($(this).data("name"));
+          chooseChar(playerChar);
+          randomCPU(cpuChar);
 
-      else if (char == "c") {
-          $("#playerPic").html('<img src="characters/andre3000.png"/>');
-          swal({   title: "You picked "+ name + "!", imageUrl: "characters/andre3000.png" });
-          
-      }
+});
+
+// var stopClick = function() {
+//           characters.unbind('click').one('click');
+//       };
+
+//       stopClick();
+
+
+
       
-      else if (char == "d") {   
-        $("#playerPic").html('<img src="characters/bigboi.png"/>');
-          swal({   title: "You picked "+ name + "!", imageUrl: "characters/bigboi.png" });
+     var chooseChar = function (char) {
+      // Selected Character send out sweet alert notifying the player,
+      // Keeps the character selection in the left column.
+      // Triggers the CPU random character selection.
+
+
+          if (char == "a") {
+              $("#playerPic").html('<img src="characters/2Chainz.png"/>');
+              swal({   
+                title: "You picked "+ name + "!"
+              , imageUrl: "characters/2Chainz.png"});
+              playGame();
+          }
+              
+              
+                                                             // .css('background-image', 'url(' + varImagePath + ')'')
+              // $(this).attr('data-charname')
              
-        }
+          else if (char == "b") {
+              $("#playerPic").html('<img src="characters/50cent.png"/>');
+              swal({   title: "You picked "+ name + "!", imageUrl: "characters/50cent.png" });
+              playGame();
 
-      else if (char == "e") {
-        $("#playerPic").html('<img src="characters/biggie.png"/>');
-           swal({   title: "You picked "+ name + "!", imageUrl: "characters/biggie.png" });
-               }   
+          }  
 
-     else if (char == "f") {
-        $("#playerPic").html('<img src="characters/busta.png"/>');
-           swal({   title: "You picked "+ name + "!", imageUrl: "characters/busta.png" });
-           
-      }
+          else if (char == "c") {
+              $("#playerPic").html('<img src="characters/andre3000.png"/>');
+              swal({   title: "You picked "+ name + "!", imageUrl: "characters/andre3000.png" });
+              playGame();
+
+          }
           
-    else if (char == "g") {
-        $("#playerPic").html('<img src="characters/common.png"/>');
-           swal({   title: "You picked "+ name + "!", imageUrl: "characters/common.png" });
-           
-      }    
+          else if (char == "d") {   
+            $("#playerPic").html('<img src="characters/bigboi.png"/>');
+              swal({   title: "You picked "+ name + "!", imageUrl: "characters/bigboi.png" });
+            playGame();
+
+            }
+
+          else if (char == "e") {
+            $("#playerPic").html('<img src="characters/biggie.png"/>');
+               swal({   title: "You picked "+ name + "!", imageUrl: "characters/biggie.png" });
+                   playGame();
+                   }   
+
+         else if (char == "f") {
+            $("#playerPic").html('<img src="characters/busta.png"/>');
+               swal({   title: "You picked "+ name + "!", imageUrl: "characters/busta.png" });
+            playGame();
       
-    else if (char == "h") {
-        $("#playerPic").html('<img src="characters/drake.png"/>'); 
-            swal({   title: "You picked "+ name + "!", imageUrl: "characters/drake.png" });
-            
-      }
-      
-    else if (char == "i") {
-        $("#playerPic").html('<img src="characters/biggie.png"/>');
-            swal({   title: "You picked "+ name + "!", imageUrl: "characters/e40.png" });
-            
-      }
-      
-    else if (char == "j") {
-        $("#playerPic").html('<img src="characters/eminem.png"/>');
-            swal({   title: "You picked "+ name + "!", imageUrl: "characters/eminem.png" });
-            
-      }
-
-    else if (char == "k") {
-        $("#playerPic").html('<img src="characters/biggie.png"/>');
-           swal({   title: "You picked "+ name + "!", imageUrl: "characters/biggie.png" });
-           
-      }
-          
-    else if (char == "l") {
-        $("#playerPic").html('<img src="characters/jadakiss.png"/>');
-           swal({   title: "You picked "+ name + "!", imageUrl: "characters/jadakiss.png" });
-           
-      }
-      
-    else if (char == "m") {
-        $("#playerPic").html('<img src="characters/jayz.png"/>');
-           swal({   title: "You picked "+ name + "!", imageUrl: "characters/jayz.png" });
-           
-      }
-
-    else if (char == "n") {
-        $("#playerPic").html('<img src="characters/jcole.png"/>');
-          swal({   title: "You picked "+ name + "!", imageUrl: "characters/jcole.png" });
-          
-      }
-
-    else if (char == "o") {
-        $("#playerPic").html('<img src="characters/kanyewest.png"/>');
-          swal({   title: "You picked "+ name + "!", imageUrl: "characters/kanyewest.png" });
-          
-      }
-
-    else if (char == "p") {
-        $("#playerPic").html('<img src="characters/kendrick.png"/>');
-         swal({   title: "You picked "+ name + "!", imageUrl: "characters/kendrick.png" });
-         
-      }
-          
-    else if (char == "q") {
-        $("#playerPic").html('<img src="characters/laurynhill.png"/>');
-         swal({   title: "You picked "+ name + "!", imageUrl: "characters/laurynhill.png" });
-         
-      }
-
-    else if (char == "r") {
-        $("#playerPic").html('<img src="characters/missyelliot.png"/>');
-         swal({   title: "You picked "+ name + "!", imageUrl: "characters/missyelliot.png" });
-         
-      }
-
-    else if (char == "s") {
-        $("#playerPic").html('<img src="characters/mosdef.png"/>');
-        swal({   title: "You picked "+ name + "!", imageUrl: "characters/mosdef.png" });
-        
-      }
-
-    else if (char == "t") {
-        $("#playerPic").html('<img src="characters/nas.png"/>');
-        swal({   title: "You picked "+ name + "!", imageUrl: "characters/nas.png" });
-        
-      }
-
-    else if (char == "u") {
-        $("#playerPic").html('<img src="characters/puffdaddy.png"/>');
-        swal({   title: "You picked "+ name + "!", imageUrl: "characters/puffdaddy.png" });
-        
-      }
-
-    else if (char == "v") {
-        $("#playerPic").html('<img src="characters/snoopextra.png"/>');
-        swal({   title: "You picked "+ name + "!", imageUrl: "characters/snoopextra.png" });
-        
-      }
-
-    else if (char == "w") {
-        $("#playerPic").html('<img src="characters/tupac.png"/>');
-        swal({   title: "You picked "+ name + "!", imageUrl: "characters/tupac.png" });
-        
-      }
-      
-    else if (char == "x") {
-        $("#playerPic").html('<img src="characters/weezy.png"/>');
-        swal({   title: "You picked "+ name + "!", imageUrl: "characters/weezy.png" });
-        
-      }   
-
-};
-
-
- //CPU randomly selects a character.
- var randomCPU = function () {
-      
-   var ran = $(".col-xs-2");
-      if (ran.length) {
-      var keep = Math.floor(Math.random() * ran.length);
-      for (var i = 0; i < ran.length; ++i) {
-      if (i !== keep) {
-          
-    }
-  }
-     console.log(ran);
-}
+          }
+              
+        else if (char == "g") {
+            $("#playerPic").html('<img src="characters/common.png"/>');
+               swal({   title: "You picked "+ name + "!", imageUrl: "characters/common.png" });
+                        playGame();
      
-}; 
+          }    
+          
+        else if (char == "h") {
+            $("#playerPic").html('<img src="characters/drake.png"/>'); 
+                swal({   title: "You picked "+ name + "!", imageUrl: "characters/drake.png" });
+                        playGame();
+      
+          }
+          
+        else if (char == "i") {
+            $("#playerPic").html('<img src="characters/biggie.png"/>');
+                swal({   title: "You picked "+ name + "!", imageUrl: "characters/e40.png" });
+                        playGame();
+      
+          }
+          
+        else if (char == "j") {
+            $("#playerPic").html('<img src="characters/eminem.png"/>');
+                swal({   title: "You picked "+ name + "!", imageUrl: "characters/eminem.png" });
+                        playGame();
+      
+          }
+
+        else if (char == "k") {
+            $("#playerPic").html('<img src="characters/biggie.png"/>');
+               swal({   title: "You picked "+ name + "!", imageUrl: "characters/biggie.png" });
+                         playGame();
+
+          }
+              
+        else if (char == "l") {
+            $("#playerPic").html('<img src="characters/jadakiss.png"/>');
+               swal({   title: "You picked "+ name + "!", imageUrl: "characters/jadakiss.png" });
+                    playGame();
+
+          }
+          
+        else if (char == "m") {
+            $("#playerPic").html('<img src="characters/jayz.png"/>');
+               swal({   title: "You picked "+ name + "!", imageUrl: "characters/jayz.png" });
+                   playGame();
+     
+          }
+
+        else if (char == "n") {
+            $("#playerPic").html('<img src="characters/jcole.png"/>');
+              swal({   title: "You picked "+ name + "!", imageUrl: "characters/jcole.png" });
+                   playGame();
+
+          }
+
+        else if (char == "o") {
+            $("#playerPic").html('<img src="characters/kanyewest.png"/>');
+              swal({   title: "You picked "+ name + "!", imageUrl: "characters/kanyewest.png" });
+                   playGame();
+
+          }
+
+        else if (char == "p") {
+            $("#playerPic").html('<img src="characters/kendrick.png"/>');
+             swal({   title: "You picked "+ name + "!", imageUrl: "characters/kendrick.png" });
+                  playGame();
+
+          }
+              
+        else if (char == "q") {
+            $("#playerPic").html('<img src="characters/laurynhill.png"/>');
+             swal({   title: "You picked "+ name + "!", imageUrl: "characters/laurynhill.png" });
+                  playGame();
+
+          }
+
+        else if (char == "r") {
+            $("#playerPic").html('<img src="characters/missyelliot.png"/>');
+             swal({   title: "You picked "+ name + "!", imageUrl: "characters/missyelliot.png" });
+                playGame();
+
+          }
+
+        else if (char == "s") {
+            $("#playerPic").html('<img src="characters/mosdef.png"/>');
+            swal({   title: "You picked "+ name + "!", imageUrl: "characters/mosdef.png" });
+                playGame();
+
+          }
+
+        else if (char == "t") {
+            $("#playerPic").html('<img src="characters/nas.png"/>');
+            swal({   title: "You picked "+ name + "!", imageUrl: "characters/nas.png" });
+                playGame();
+
+          }
+
+        else if (char == "u") {
+            $("#playerPic").html('<img src="characters/puffdaddy.png"/>');
+            swal({   title: "You picked "+ name + "!", imageUrl: "characters/puffdaddy.png" });
+                playGame();
+
+          }
+        else if (char == "v") {
+            $("#playerPic").html('<img src="characters/snoopextra.png"/>');
+            swal({   title: "You picked "+ name + "!", imageUrl: "characters/snoopextra.png" });
+                      playGame() ;
+          }
+
+        else if (char == "w") {
+            $("#playerPic").html('<img src="characters/tupac.png"/>');
+            swal({   title: "You picked "+ name + "!", imageUrl: "characters/tupac.png" });
+                      playGame();
+
+          }
+          
+        else if (char == "x") {
+            $("#playerPic").html('<img src="characters/weezy.png"/>');
+            swal({   title: "You picked "+ name + "!", imageUrl: "characters/weezy.png" });
+                      playGame();          }   
+
+      };
+   
+});
+
+        //CPU randomly selects a character.
+       var randomCPU = function (char) {
+
+            var random = Math.floor(Math.random() * 23);
+            char = $(".col-xs-2");
+            cpuChar= char.eq(random % char.length).addClass("cpuChar").attr("id");
+            return cpuChar;
+
+         // var char = $(".col-xs-2");
+         //    if (char.length) {
+         //    var keep = Math.floor(Math.random() * char.length);
+         //    for (var i = 0; i < char.length; ++i) {
+         //    if (i !== keep) {
+         //        playGame() cpuChar;
+          //}
+        };
 
 
+      //check if player and CPU selected same character, if so, excecute randomoCPU again
+      var checkSelect = function () {
+            if (cpuChar === playerChar) {
+              randomCPU();
+            }
+     };
 
- }); 
- 
+      //Play the game.
+    function playGame() {            
+
+
+                  if (this === cpuChar) {
+                    alert("You beat CPU!");
+                  }
+                  
+                  characters.on('click',function() {
+                    $('.col-xs-2 > *').toggle();
+                    $(this).show();
+                    return false;
+
+                  });
+
+
+                  
+                           
+                  };
+           
+
+});
+     
+
   //Scrollbar//
   $( "#trigger" ).ready(function() {
     $( "#questions" ).scroll();
